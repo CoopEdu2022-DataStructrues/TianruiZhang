cab921b9e07087e8aceea808d4dad474ac165d8b
GaryZhang123 <ztrgary@outlook.com>
2023-02-10 21:05:56 +0800

    hello

--- U1-L1/1.1.6.cpp
+++ U1-L1/1.1.6.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 int main(){
-    double a=0,b=0,c=0,d=0,e=0;
+    double a=0;
     for (int i=1;i<10;i++){
         for (int k=0;k<10;k++){
             for (int p=0;p<10;p++){
