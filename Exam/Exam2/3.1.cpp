ef957506570d51ce0ca0d47ee5da53307c15fd29
GaryZhang123 <ztrgary@outlook.com>
2023-02-20 17:14:15 +0800

    hello

--- Exam/Exam2/3.1.cpp
+++ Exam/Exam2/3.1.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 int main(){
 
-    int answer=0,tou=0,jiao=0,sum;
-    cin>>tou>>jiao>>sum;
+    int answer=0,tou=0,jiao=0;
+    cin>>tou>>jiao;
     if (((tou+jiao)/2)*2!=(tou+jiao)){
         cout<<"-1";
     }else{
-        answer=(sum-4*tou)/2;
+        answer=(jiao-4*tou)/2;
         if ((answer/2)*2!=answer){
             cout<<"-1";
         }
