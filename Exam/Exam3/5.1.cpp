295c76bab8fb87bcd4e9265e8e0cbe469917cc25
GaryZhang123 <ztrgary@outlook.com>
2023-03-09 10:58:51 +0800

    hello

---
+++ Exam/Exam3/5.1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){}
+
+struct ListNode {
+    double value = 0;
+    ListNode *next = nullptr;
+};
+
+
+
---
+++ Exam/Exam3/5.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+struct ListNode {
+    int value = 0;
+    ListNode *next = nullptr;
+};
+
+ListNode * InitLinkedList() {
+    ListNode *L = new ListNode;
+    return L;
+}
+
+bool Init(int *a){
+
+}
+int main(){
+
+}
